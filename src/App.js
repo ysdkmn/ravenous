f0286a37aaa28f748c0f00d529c14b14b62a5187
ysdkmn <ysdkmn@gmail.com>
2018-02-13 12:16:05 -0500

    complete SearchBar component and render components through App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
 
-class App extends Component {
+class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div className="App">
+      <h1>ravenous</h1>
+      <SearchBar/>
+      <BusinessList/>
+    </div>);
   }
 }
 
