8dbc702e8adab781fcbff1a0a0b392533a70579b
ysdkmn <ysdkmn@gmail.com>
2018-03-06 10:37:03 -0500

    fix error setState in render by repkacing pageLoadSearch with componentWillMount

--- src/App.js
+++ src/App.js
@@ -9,26 +9,20 @@ class App extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        businesses: [ ],
-        initial_load: false
+        businesses: []
       };
       this.searchYelp=this.searchYelp.bind(this);
-      this.pageLoadSearch=this.pageLoadSearch.bind(this);
   }
 
   searchYelp(term, location, sortBy) {
     Yelp.search(term,location,sortBy).then( businesses => this.setState({businesses: businesses}));
   }
 
-  pageLoadSearch() {
-    if (!this.state.initial_load) {
-      this.searchYelp(" ", "New York", "rating");
-      this.setState({initial_load: true});
-    }
+  componentWillMount() {
+    this.searchYelp(" ", "New York", "rating");
   }
 
   render() {
-    this.pageLoadSearch();
     return (<div className="App">
       <h1>ravenous</h1>
       <SearchBar searchYelp={this.searchYelp} />
