d31042ab4e3f1a8efaf12249c302ae6be57f866f
ysdkmn <ysdkmn@gmail.com>
2018-03-09 14:53:33 -0500

    add utils

---
+++ src/util/GoogleMaps.js
@@ -0,0 +1,45 @@
+const apiKey = "AIzaSyBRBt5EqVpvPvSHr9qBpYcgxKN9qNkdWP0";
+const searchBaseUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?rankby=distance&keyword=";
+const detailsBaseURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?placeid=";
+
+
+const GoogleMaps = {
+  async search(name, latitude, longitude) {
+      let id = await this.searchId(name, latitude, longitude);
+      if (id) {
+        return this.searchWebPage(id);
+      }
+    },
+
+  async searchId(name, latitude, longitude) {
+    try {
+      let response = await fetch(`${searchBaseUrl}${name}&location=${latitude},${longitude}&key=${apiKey}`);
+      if (response.ok) {
+        let jsonResponse = await response.json();
+        if (jsonResponse.status === "OK") {
+          return jsonResponse.results[0].place_id;
+        } else {
+          return false
+        }
+      }
+      throw new Error('Request failed!');
+    } catch(error) {
+      console.log(error);
+    }
+  },
+
+  async searchWebPage(placeId) {
+    try {
+      let response = await fetch(`${detailsBaseURL}${placeId}&key=${apiKey}`);
+      if (response.ok) {
+        let jsonResponse = await response.json()
+        return jsonResponse.result.website;
+      }
+      throw new Error('Request failed!');
+    } catch(error) {
+      console.log(error);
+    }
+  }
+}
+
+export default GoogleMaps;
---
+++ src/util/Yelp.js
@@ -0,0 +1,42 @@
+const apiKey = "yu_a7NKXDSsj72pao70LOuGPVKutq8oJHO14acF5jxC-njefi8QEp2KMNvRQGZYKRUMaViYJvQI0rGaOPrjakANcpu9mXwIkJ-LYq-2_XVoRS7ulk9KTYiNn7Y-dWnYx";
+
+const Yelp = {
+  async search(term, location, sortBy) {
+    try {
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&categories=food,restaurants&sort_by=${sortBy}`, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      });
+      if (response.ok) {
+        let jsonResponse = await response.json();
+        if (jsonResponse.businesses) {
+          console.log('YelpSearch');
+          return jsonResponse.businesses.map(business => {
+            return ({
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+              latitude: business.coordinates.latitude,
+              longitude: business.coordinates.longitude,
+              url: business.url,
+              googleMapUrl: `https://www.google.com/maps/search/?api=1&query=${business.name}`
+            });
+          });
+        }
+      }
+      throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+  }
+}
+}
+
+export default Yelp;
